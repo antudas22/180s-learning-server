924b749dabac85a4c75ef245118f657b8edfb3a3
antudas22 <20antu20@gmail.com>
2022-10-26 01:15:17 +0600

    data load completed

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const courses = require('./data/courses.json');
 
 app.get('/', (req, res) => {
     res.send('Our Course API');
@@ -14,6 +15,23 @@ app.get('/', (req, res) => {
 app.get('/course-categories', (req, res) => {
     res.send(categories)
 });
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if(id === "07"){
+        res.send(courses);
+    }
+    else{
+        const categoryCourses = courses.filter(c => c.category_id === id);
+        res.send(categoryCourses);
+    }
+})
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourses = courses.find(course => course._id === id);
+    res.send(selectedCourses);
+})
 
 app.listen(port, () => {
     console.log('180s Learning', port);
