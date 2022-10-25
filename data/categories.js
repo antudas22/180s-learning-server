d1f97aa3f8a97a6cbfeba8d0b6900db13e006c1d
antudas22 <20antu20@gmail.com>
2022-10-25 20:06:50 +0600

    categories data added

---
+++ data/categories.js
@@ -0,0 +1,30 @@
+[
+    {
+        "id": "01",
+        "name": "HTML"
+    },
+    {
+        "id": "02",
+        "name": "CSS"
+    },
+    {
+        "id": "03",
+        "name": "Bootstrap"
+    },
+    {
+        "id": "04",
+        "name": "JavaScript"
+    },
+    {
+        "id": "05",
+        "name": "React"
+    },
+    {
+        "id": "06",
+        "name": "NodeJS"
+    },
+    {
+        "id": "07",
+        "name": "All Courses"
+    }
+]
